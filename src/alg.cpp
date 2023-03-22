a05d7dbd784706c1eca926d8399c92eda30a05b6
mellyexe <125124784+mellyexe@users.noreply.github.com>
2023-03-22 23:34:19 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,5 @@
 // Copyright 2021 NNTU-CS
-int cbinsearch(int* arr, int size, int value) 
-{
+int cbinsearch(int* arr, int size, int value) {
   int count = 0;
   int left = 0, right = size - 1;
   int mid = 0;
@@ -20,17 +19,16 @@ int cbinsearch(int* arr, int size, int value)
         temp++;
       }
       return count;
-    }
-    else if (arr[mid] < value)
-      left = mid + 1;
-    else if (arr[mid] > value)
-      right = mid - 1;
+    } else if (arr[mid] < value) {
+        left = mid + 1;
+      } else if (arr[mid] > value) {
+          right = mid - 1;
+        }
   }
   return 0;
 }
 
-int countPairs1(int* arr, int len, int value)
-{
+int countPairs1(int* arr, int len, int value) {
   int count = 0;
   for (int i = 0; i < len - 1; i++) {
     for (int j = i + 1; j < len; j++) {
@@ -40,8 +38,7 @@ int countPairs1(int* arr, int len, int value)
   return count;
 }
 
-int countPairs2(int* arr, int len, int value)
-{
+int countPairs2(int* arr, int len, int value) {
   int count = 0;
   for (int i = 0; i < len - 1; i++) {
     for (int j = i + 1; j < len && arr[i] + arr[j] <= value; j++) {
@@ -52,8 +49,7 @@ int countPairs2(int* arr, int len, int value)
   return count;
 }
 
-int countPairs3(int* arr, int len, int value)
-{
+int countPairs3(int* arr, int len, int value) {
   int count = 0;
   for (int i = 0; i < len - 1 && arr[i] <= value; i++)
     count += cbinsearch(arr + i + 1, len - i - 1, value - arr[i]);
